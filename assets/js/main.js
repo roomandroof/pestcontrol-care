ce10560f7eea605e03648cf46033b38c838a2cfa
jeromymargrett06-collab <fbadsaccess3016@gmail.com>
2026-01-16 23:24:07 +0530

    na

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-
-// Smooth scroll for "Get a Free Quote"
-document.addEventListener('click', e => {
-  const a = e.target.closest('a[href^="#"]');
-  if (!a) return;
-  const el = document.querySelector(a.getAttribute('href'));
-  if (el){ e.preventDefault(); el.scrollIntoView({behavior:'smooth'}); }
-});
